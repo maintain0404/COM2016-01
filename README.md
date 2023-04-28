897109ad1f24b454f46d99231586d216dd203166
Taein Min <maintain0404@gmail.com>
2023-04-28 08:10:26 +0000

    🎉 init

---
+++ README.md
@@ -0,0 +1,3 @@
+# COM2016-01
+
+This is repository for the lecture SKKU Linux System(COM2016-01) source codes.
---
+++ practice/src/test_cpp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    cout << "Hello World2\n";
+}
